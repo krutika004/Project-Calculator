f5a5120d11dfd88ffbfaafd9a3d4d907dbd8ab1a
Krutika <01fe22bca052@kletech.ac.in>
2024-02-08 20:18:37 +0530

    Mathematical operations

---
+++ MathematicalOp.java
@@ -0,0 +1,15 @@
+import java.lang.Math;
+public class MathematicalOp
+{    
+    public static void main(String[] args)     
+    {    
+        double x = 20;    
+        double y = 2;    
+        System.out.println("Maximum number of x and y is: " +Math.max(x, y));   
+        System.out.println("Square root of y is: " + Math.sqrt(y));     
+        System.out.println("Power of x and y is: " + Math.pow(x, y));          
+        System.out.println("Logarithm of x is: " + Math.log(x));   
+        System.out.println("Exp of a is: " +Math.exp(x));    
+        System.out.println("Absolute value of a is: " +Math.abs(x));  
+    }    
+}    
